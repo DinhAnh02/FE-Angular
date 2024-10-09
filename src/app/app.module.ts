17e6c1c8fb8698ad91c66b678e4f57e24340663e
DinhAnh02 <danh28902@gmail.com>
2024-10-09 10:09:30 +0700

    gg

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { MidFooterComponent } from './mid-footer/mid-footer.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { OrderComponent } from './order/order.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    MidFooterComponent,
+    OrderComponent,
+    OrderConfirmComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+  ],
+  providers: [],
+  bootstrap: [
+    // HeaderComponent,
+    // HomeComponent,
+    // FooterComponent,
+    // MidFooterComponent,
+    // MidHeaderComponent,
+    // OrderComponent,
+    // OrderConfirmComponent,
+    LoginComponent,
+  ]
+})
+export class AppModule { }
---
+++ src/app/mid-footer/mid-footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mid-footer',
+  templateUrl: './mid-footer.component.html',
+  styleUrl: './mid-footer.component.scss'
+})
+export class MidFooterComponent {
+
+}
